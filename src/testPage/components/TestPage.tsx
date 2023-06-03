2e47bf0fe0c9a4119267a9bde273b0842e250066
a.gerard <a.gerard@sesamm.com>
2023-06-04 01:04:26 +0200

    feat: Add contexts

--- src/testPage/components/TestPage.tsx
+++ src/testPage/components/TestPage.tsx
@@ -1,9 +1,17 @@
-import { Typography } from "@mui/material";
-import React, { FC } from "react";
+import { ConstantsContext } from "@/constants";
+import styled from "@emotion/styled";
+import { Typography, useTheme } from "@mui/material";
+import React, { FC, useContext } from "react";
 import { useTranslation } from "react-i18next";
 
 export const TestPage: FC = () => {
+  const theme = useTheme();
   const { t } = useTranslation("TestPage");
+  const appConstants = useContext(ConstantsContext);
 
-  return <Typography fontWeight={700}> {t("title")}</Typography>;
+  const Text = styled(Typography)`
+    color: ${theme.app.testPage.textColor};
+  `;
+
+  return <Text>{t(appConstants.testPage.title)}</Text>;
 };
