1bb51c7d47d30adb572dbb5372bd016812e518fb
a.gerard <a.gerard@sesamm.com>
2023-11-21 12:44:01 +0100

    feat: Rework the boilerplate

--- src/landingPage/components/LandingPage.tsx
+++ src/landingPage/components/LandingPage.tsx
@@ -1,19 +1,19 @@
 import React, { FC } from "react";
 import { useTranslation } from "react-i18next";
 
-import { ButtonVariants, TypographyVariants } from "@/theme";
-import { Button, Typography } from "@mui/material";
 import { FullLogo } from "@/core/assets";
 import { useLanguage } from "@/language";
+import { ButtonVariants, TypographyVariants } from "@/theme";
+import { Button, Typography } from "@mui/material";
 
 export const LandingPage: FC = () => {
   const { t } = useTranslation("LandingPage");
   const { language } = useLanguage();
 
   return (
-    <div>
-      <FullLogo />
-      <Typography variant={TypographyVariants.TITLE}>{t("title")}</Typography>;
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: 20, margin: 40 }}>
+      <img src={FullLogo} style={{ width: 120 }} />
+      <Typography variant={TypographyVariants.TITLE}>{t("title")}</Typography>
       <Button variant={ButtonVariants.TEXT}>{language}</Button>
     </div>
   );
